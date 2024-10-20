3dfca907a75f12de978310a58fbed79d839a4fef
hijiki51 <hibiki0719euph@gmail.com>
2024-10-21 00:45:44 +0900

    feat: 🎸 add network setting

--- modules/chapter4/main.tf
+++ modules/chapter4/main.tf
@@ -1,5 +1,13 @@
 
+resource "google_compute_network" "this" {
+  name = "naro-ch4-vpc"
+}
 
+resource "google_compute_subnetwork" "this" {
+  name          = "naro-ch4-subnet"
+  ip_cidr_range = "192.168.0.0/16"
+  network       = google_compute_network.this.id
+}
 
 module "paricipants" {
   count  = length(var.users)
